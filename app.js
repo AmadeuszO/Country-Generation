41fc6510f655088026f84efde32d1383b5c7cfe3
AmadeuszO <amadi9205@gmail.com>
2023-11-17 09:05:04 +0100

    final version mobile

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ btnSearch.addEventListener('click', () => {
 	fetch(url)
 		.then(res => res.json())
 		.then((data) => {
-			// console.log(data[0])
-			// console.log(data[0].capital[0])
-			// console.log(data[0].flags.svg)
-			// console.log(data[0].name.common)
-			// console.log(data[0].continents[0])
-			// console.log(Object.keys(data[0].currencies)[0])
-			// console.log(data[0].currencies[Object.keys(data[0].currencies)].name)
-			// console.log(Object.value(data[0].languages).toString().split(',').join(', '))
 			result.innerHTML = `
 			<img src="${data[0].flags.svg}" class="flag-img">
 			<h2>${data[0].name.common}</h2>
@@ -41,13 +33,18 @@ btnSearch.addEventListener('click', () => {
 			<div class="wrapper">
 				<div class="data-wrapper">
 					<h4>Currency:</h4>
-					<span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies[0])}</span>
+					<span>${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</span>
 				</div>
 			</div>
 			<div class="wrapper">
 				<div class="data-wrapper">
-					<h4>Common Languages:</h4>
-					<span>${Object.values(data[0].languages).toString().split(',').join(', ')}</span>
+					<h4>Languages:</h4>
+					<span>
+					${Object.values(data[0].languages)
+                		.toString()
+                		.split(',')
+                		.join(', ')}
+					</span>
 				</div>
 			</div>
 `
